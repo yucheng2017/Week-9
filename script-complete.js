88c0b6f2a2057a49cee2d850401b8e799519e64e
Navarjun <navarjunsingh@gmail.com>
2017-03-22 02:33:58 -0400

    Class files

---
+++ script-complete.js
@@ -0,0 +1,15 @@
+var highlighterDiv = document.getElementById('highlighter');
+var triggers = document.querySelectorAll("div img");
+
+for (var i = 0; i < triggers.length; i++) {
+  trigger = triggers[i];
+  trigger.addEventListener("mouseenter", function(e) {
+    var coords = this.getBoundingClientRect();
+    highlighterDiv.style.top = coords.top+"px";
+    highlighterDiv.style.left = coords.left+"px";
+
+    highlighterDiv.style.width = coords.width+"px";
+    highlighterDiv.style.height = coords.height+"px";
+    highlighterDiv.style.opacity = 1;
+  });
+}
